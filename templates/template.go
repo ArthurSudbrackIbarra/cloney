68b4a00e45300433f3ae3103f9fea614739ac59e
MagicManatee <arthur.ibarra@edu.pucrs.br>
2023-10-02 12:25:02 -0300

    Out of scope file create validation.

--- templates/template.go
+++ templates/template.go
@@ -27,12 +27,12 @@ func NewTemplateFiller(variablesMap map[string]interface{}) *TemplateFiller {
 }
 
 // injectCustomToFileFuncPaths takes a file path, its content, and a flag indicating whether the output
-// is intended for the terminal. It returns a modified version of the file content with the first hidden
-// parameter of the 'toFile' functions injected. This hidden parameter represents the directory of the file
-// being processed and is used to calculate the absolute path of files created relative to it.
+// is intended for the terminal. It returns a modified version of the file content with the two first hidden
+// parameter of the 'toFile' functions injected.
+//
 // If 'outputInTerminal' is true, an error is returned because the 'toFile' function is not supported in
 // terminal output mode; it should be used with 'cloney dry-run -o <output_directory>' instead.
-func injectCustomToFileFuncPaths(filePath string, fileContent string, outputInTerminal bool) (string, error) {
+func injectCustomToFileFuncPaths(templateDir, filePath, fileContent string, outputInTerminal bool) (string, error) {
 	// Split the template content into lines for processing.
 	fileLines := strings.Split(fileContent, "\n")
 
@@ -48,9 +48,7 @@ func injectCustomToFileFuncPaths(filePath string, fileContent string, outputInTe
 			// This parameter is the directory of the file being processed.
 			regex := regexp.MustCompile(`{{-? ?toFile`)
 			fileDir := filepath.Dir(filePath)
-			newLine := regex.ReplaceAllString(line, fmt.Sprintf("{{- toFile \"%s\"", fileDir))
-
-			fmt.Println(newLine)
+			newLine := regex.ReplaceAllString(line, fmt.Sprintf("{{- toFile \"%s\" \"%s\"", templateDir, fileDir))
 
 			// If on Windows, replace backslashes with forward slashes.
 			if os.PathSeparator == '\\' {
@@ -84,7 +82,7 @@ func (t *TemplateFiller) FillDirectory(src string, ignorePaths []string, outputI
 		}
 
 		// Get a new version of the file content with the first hidden parameter of the 'toFile' function injected.
-		fileContent, err := injectCustomToFileFuncPaths(filePath, string(fileBytes), outputInTerminal)
+		fileContent, err := injectCustomToFileFuncPaths(src, filePath, string(fileBytes), outputInTerminal)
 		if err != nil {
 			return err
 		}
@@ -154,7 +152,7 @@ func (t *TemplateFiller) CreateFilledDirectory(src string, dest string, ignorePa
 		}
 
 		// Get a new version of the file content with the first hidden parameter of the 'toFile' function injected.
-		fileContent, err := injectCustomToFileFuncPaths(targetFilePath, string(fileBytes), false)
+		fileContent, err := injectCustomToFileFuncPaths(dest, targetFilePath, string(fileBytes), false)
 		if err != nil {
 			return err
 		}
