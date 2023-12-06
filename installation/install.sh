845b6c354288015cb96feab51b99272e8b3f4634
MagicManatee <arthur.ibarra@edu.pucrs.br>
2023-12-06 18:09:40 -0300

    Update script.

--- installation/install.sh
+++ installation/install.sh
@@ -9,8 +9,8 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 # Check if this script is being run as root.
-if [ "$EUID" -ne 0 ]
-then echo -e "${RED}Error: Please run this script as root.${NC}"
+if [ "$EUID" -ne 0 ]; then
+    echo -e "${RED}Error: Please run this script as root.${NC}"
     exit 1
 fi
 
@@ -21,20 +21,16 @@ echo "Operating System: $OPERATING_SYSTEM"
 echo "Architecture: $ARCHITECTURE"
 
 # Define file name according to OS and Architecture.
-if [ "$OPERATING_SYSTEM" = "linux" ] && { [ "$ARCHITECTURE" = "x86_64" ] || [ "$ARCHITECTURE" = "amd64" ]; }
-then
+if [ "$OPERATING_SYSTEM" = "linux" ] && { [ "$ARCHITECTURE" = "x86_64" ] || [ "$ARCHITECTURE" = "amd64" ]; }; then
     FILE_NAME="cloney-linux-amd64"
-elif [ "$OPERATING_SYSTEM" = "linux" ] && { [ "$ARCHITECTURE" = "aarch64" ] || [ "$ARCHITECTURE" = "arm64" ]; }
-then
+elif [ "$OPERATING_SYSTEM" = "linux" ] && { [ "$ARCHITECTURE" = "aarch64" ] || [ "$ARCHITECTURE" = "arm64" ]; }; then
     FILE_NAME="cloney-linux-arm64"
-elif [ "$OPERATING_SYSTEM" = "darwin" ] && { [ "$ARCHITECTURE" = "x86_64" ] || [ "$ARCHITECTURE" = "amd64" ]; }
-then
+elif [ "$OPERATING_SYSTEM" = "darwin" ] && { [ "$ARCHITECTURE" = "x86_64" ] || [ "$ARCHITECTURE" = "amd64" ]; }; then
     FILE_NAME="cloney-darwin-amd64"
-elif [ "$OPERATING_SYSTEM" = "darwin" ] && { [ "$ARCHITECTURE" = "aarch64" ] || [ "$ARCHITECTURE" = "arm64" ]; }
-then
+elif [ "$OPERATING_SYSTEM" = "darwin" ] && { [ "$ARCHITECTURE" = "aarch64" ] || [ "$ARCHITECTURE" = "arm64" ]; }; then
     FILE_NAME="cloney-darwin-arm64"
 else
-    echo -e "${RED}Error: Unsupported Operating System and/or Architecture.${NC}"
+    echo -e "${RED}Error: Unsupported operating system and/or architecture.${NC}"
     exit 1
 fi
 
@@ -44,23 +40,35 @@ BINARY_LOCATION="/usr/local/bin/cloney"
 
 # Download Cloney Zip.
 curl -A "Cloney Download Script" -OL \
-"https://github.com/ArthurSudbrackIbarra/cloney/releases/download/$CLONEY_VERSION/$FILE_NAME.zip"
-echo "Downloaded Cloney $CLONEY_VERSION."
+    "https://github.com/ArthurSudbrackIbarra/cloney/releases/download/$CLONEY_VERSION/$FILE_NAME.zip" ||
+    {
+        echo -e "${RED}Error: Failed to download Cloney. Please check your internet connection.${NC}"
+        exit 1
+    }
 
 # Unzip Cloney.
-unzip -o $FILE_NAME.zip
-echo "Unzipped Cloney $CLONEY_VERSION."
+unzip -o "$FILE_NAME.zip" ||
+    {
+        echo -e "${RED}Error: Failed to unzip Cloney. Please install the zip package.${NC}"
+        exit 1
+    }
 
 # Move Cloney to /usr/local/bin.
-mv -f $FILE_NAME/cloney $BINARY_LOCATION
-echo "Moved Cloney $CLONEY_VERSION to $BINARY_LOCATION."
+mv -f "$FILE_NAME/cloney" $BINARY_LOCATION ||
+    {
+        echo -e "${RED}Error: Failed to move Cloney to $BINARY_LOCATION.${NC}"
+        exit 1
+    }
 
 # Make Cloney executable.
-chmod +x $BINARY_LOCATION
+chmod +x $BINARY_LOCATION ||
+    {
+        echo -e "${RED}Error: Failed to make Cloney binary executable.${NC}"
+        exit 1
+    }
 
-# Remove Trash.
-rm -rf $FILE_NAME.zip $FILE_NAME
-echo "Removed trash from installation."
+# Remove trash.
+rm -rf "$FILE_NAME.zip" $FILE_NAME
 
 echo
 echo -e "${GREEN}Cloney $CLONEY_VERSION was successfully installed!${NC}"
